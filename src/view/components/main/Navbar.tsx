072bb145f9591817b30789983501598130733e8a
zonaetmunna <zonaetmonna@gmail.com>
2023-04-11 22:00:04 +0600

    add

--- src/view/components/main/Navbar.tsx
+++ src/view/components/main/Navbar.tsx
@@ -1,97 +1,107 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
     <nav className="bg-white shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
-            <a href="/" className="text-xl font-bold text-gray-800">
-              Ecommerce
-            </a>
+            <Link to="/">
+              <img className="h-8 w-auto" src="/logo.png" alt="Logo" />
+            </Link>
           </div>
-          <div className="flex items-center sm:hidden">
+          <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+            <Link
+              to="/"
+              className="text-gray-900 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Home
+            </Link>
+            <Link
+              to="/shop"
+              className="text-gray-900 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Shop
+            </Link>
+            <Link
+              to="/about"
+              className="text-gray-900 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+            >
+              About
+            </Link>
+            <Link
+              to="/contact"
+              className="text-gray-900 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Contact
+            </Link>
+          </div>
+          <div className="-mr-2 flex items-center sm:hidden">
             <button
-              onClick={() => setIsOpen(!isOpen)}
+              onClick={toggleMenu}
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-500 hover:text-gray-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-600"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500"
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
               <span className="sr-only">Open main menu</span>
-              <svg
-                className={`${isOpen ? "hidden" : "block"} h-6 w-6`}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              <svg
-                className={`${isOpen ? "block" : "hidden"} h-6 w-6`}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              {isOpen ? (
+                <FiX className="block h-6 w-6" aria-hidden="true" />
+              ) : (
+                <FiMenu className="block h-6 w-6" aria-hidden="true" />
+              )}
             </button>
-          </div>
-          <div className="hidden sm:flex sm:items-center">
-            <a
-              href="/"
-              className="px-3 py-2 rounded-md text-sm font-medium text-gray-500 hover:text-gray-600 hover:bg-gray-100"
-            >
-              Home
-            </a>
-            <a
-              href="/"
-              className="px-3 py-2 rounded-md text-sm font-medium text-gray-500 hover:text-gray-600 hover:bg-gray-100"
-            >
-              Shop
-            </a>
-            <a
-              href="/"
-              className="px-3 py-2 rounded-md text-sm font-medium text-gray-500 hover:text-gray-600 hover:bg-gray-100"
-            >
-              Cart
-            </a>
           </div>
         </div>
       </div>
 
-      <div
-        className={`${
-          isOpen ? "block" : "hidden"
-        } sm:hidden bg-white px-2 pt-2 pb-3`}
-        id="mobile-menu"
-      >
-        <a
-          href="/"
-          className="block px-3 py-2 rounded-md text-base font-medium text-gray-500 hover:text-gray-600 hover:bg-gray-100"
-        >
-          Home
-        </a>
-        <a href="/" className="block px-3 py-2 rounded-md text-base font">
-          about
-        </a>
-      </div>
+      {isOpen && (
+        <div className="sm:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            <Link
+              to="/"
+              className="text-gray-900 hover:text-gray-500 block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Home
+            </Link>
+            <Link
+              to="/shop"
+              className="text-gray-900 hover:text-gray-500 block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Shop
+            </Link>
+            <Link
+              to="/about"
+              className="text-gray-900 hover:text-gray-500 block px-3 py-2 rounded-md text-base font-medium"
+            >
+              About
+            </Link>
+            <Link
+              to="/contact"
+              className="text-gray-900 hover:text-gray-500 block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Contact
+            </Link>
+            <Link
+              to="/dashboard"
+              className="text-gray-900 hover:text-gray-500 block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Dashboard
+            </Link>
+            <Link
+              to="/login"
+              className="text-gray-900 hover:text-gray-500 block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Login
+            </Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
---
+++ src/view/pages/main/Register/MarchentRegister.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MarchentRegister = () => {
+  return <div></div>;
+};
+
+export default MarchentRegister;
